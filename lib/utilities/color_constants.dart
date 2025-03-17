61e15a85f9a9968ab212270e9d7769a5d47dbe0d
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2025-03-17 03:13:31 +0100

    Added asset images for the web version of the application and also added a static class that holds all the colors that will be used consistently throughout the development of the application.

---
+++ lib/utilities/color_constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ColorConstantsDarkMode {
+  ColorConstantsDarkMode._();
+
+  static final primaryColor = Color.fromARGB(255, 8, 9, 57);
+  static final secondaryColor_1 = const Color.fromARGB(255, 24, 128, 224);
+  static final secondaryColor_2 = const Color.fromARGB(255, 15, 15, 47);
+  static final tertiaryColor = const Color.fromARGB(255, 255, 255, 255);
+  static final gradientColor1 = const Color.fromARGB(224, 7, 139, 126);
+}
+
+
+class ColorConstantsLightMode {
+  ColorConstantsLightMode._();
+  static final primaryColor = Color.fromARGB(255, 28, 68, 148);
+  static final secondaryColor_1 = const Color.fromARGB(255, 86, 130, 215);
+  static final secondaryColor_2 = const Color.fromARGB(255, 141, 176, 244);
+  static final tertiaryColor = const Color.fromARGB(255, 0, 0, 0);
+  static final gradientColor1 = const Color.fromARGB(224, 7, 139, 126);
+}
